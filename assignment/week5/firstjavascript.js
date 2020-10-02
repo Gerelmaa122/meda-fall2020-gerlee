d08c856a5d80a18b227f2ec47f197865ad5e5e70
Jenny <gerelmaapurevtseren@gmail.com>
2020-10-01 18:45:23 -0700

    Javascript

--- assignment/week5/firstjavascript.js
+++ assignment/week5/firstjavascript.js
@@ -1,3 +1,81 @@
 console.log("hello world!");
 console.log("how are you doing");
 console.log("I'm eating pasta,I love it");
+// variable
+var myBox;
+//Naming Schemes
+//camel case:captilize every word After the first word.
+//Example:originalLocation
+//Shake Case:every spaceis replaced by an underscore,no capital letters.
+//example:original_location
+myBox="kitchen utensils";
+var secondvariable="bathroom supplies"
+myBox="books"
+console.log(myBox);
+// Matematical Operations
+// +,-,?,*,%
+  //  var sum = 2 + 4;
+  //  var difference = 2 - 4;
+  //  var quotient = 2 / 4;
+  //  var product = 2 * 4;
+  //  var remainder = 2 % 4;
+  //  console.log("The Quotient of 2 and 4 is", quotient);
+   // console.log(remainder);
+    
+    // Logical Operator
+    // ==,>,<,!=,<=,>=
+  //  var isIRaining = true;
+   // isIRaining == true;
+  //  var someNumber = 100;
+   // somenumber == 50;//false
+   // somenumber >= 50;//true
+   // somenumber < 50;//false
+
+//"hello"=="hello";//true
+
+//sum > product;
+
+    //If Statement,is to run code based on existing data,
+    //if this is true     the run this
+    //if     ()                  {}
+
+  if(false) {
+   console.log("This is true!");
+console.log("We march on!");
+}
+var personAge=21;
+  if (personAge >= 21) {
+    console.log("Congrats you can drive!");
+} else {
+    console.log("Sorry,you are too young to drive");
+}
+console.log("We march on!");
+
+// Function 
+// keyword     name of function    code block
+// function   myFunction()        {}
+
+function myFirstfunction() {
+ 
+    console.log("Have a nice day!");
+    console.log("Today will be sunny");
+    console.log("Today will be dry");
+    
+    if(raining) {
+        console.log("bring an umberella");
+    }
+    var raining=true;
+}
+myFirstfunction();
+myFirstfunction();
+myFirstfunction();
+
+// Practice creating the following:
+/*
+variables
+Datatypes
+Functions
+Logival Comparison
+*/
+
+
